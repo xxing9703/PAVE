6a9d3ea8e138d0f551723240d3ec6fba3cfff1a5
Xi Xing <xxing@princeton.edu>
2018-09-04 13:56:14 -0400

    Update PAVE_stat.m

--- PAVE_stat.m
+++ PAVE_stat.m
@@ -1,11 +1,10 @@
- %convert to table and export
-pks_tb.feature(cellfun(@isempty,pks_tb.feature)==1)={'Unknown'}; % fill [] with unknown
-[cats, ~, J]=unique(pks_tb.feature);
+% ------stat on feature
+[cats,~,J]=unique({pks.feature});
 occ = histc(J, 1:numel(cats));
-stat_feature=[cats,num2cell(occ)];  %show statistics 
+stat_feature=[cats',num2cell(occ)];
 
-
-pks_tb.description(cellfun(@isempty,pks_tb.description)==1)={'N/A'};
-[cats, ~, J]=unique(pks_tb.description);
+% ---------stat on all adducts
+tp=pks(find(~cellfun(@isempty,{pks.description})));
+[cats, ~, J]=unique({tp.description});
 occ = histc(J, 1:numel(cats));
-stat_add=[cats,num2cell(occ)];  %show statistics 
+stat_add=[cats',num2cell(occ)];
